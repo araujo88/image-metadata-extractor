33d8e6e0263a8e238c76ebdfa363a7d663acaf47
Leonardo de Araujo <46436462+araujo88@users.noreply.github.com>
2023-01-20 00:57:51 -0300

    Add files via upload

---
+++ src/main/java/app/App.java
@@ -0,0 +1,85 @@
+package app;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+//import com.drew.imaging.jpeg.JpegMetadataReader;
+//import com.drew.imaging.jpeg.JpegProcessingException;
+//import com.drew.imaging.jpeg.JpegSegmentMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+//import com.drew.metadata.exif.ExifReader;
+//import com.drew.metadata.iptc.IptcReader;
+
+import java.io.File;
+import java.io.IOException;
+//import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws ImageProcessingException, IOException {
+        if (args.length != 1) {
+            System.err.println("Expected a single argument of the path to the images to be processed.");
+            System.exit(1);
+        }
+
+        File file = new File(args[0]);
+
+        // There are multiple ways to get a Metadata object for a file
+
+        //
+        // SCENARIO 1: UNKNOWN FILE TYPE
+        //
+        // This is the most generic approach. It will transparently determine the file
+        // type and invoke the appropriate
+        // readers. In most cases, this is the most appropriate usage. This will handle
+        // JPEG, TIFF, GIF, BMP and RAW
+        // (CRW/CR2/NEF/RW2/ORF) files and extract whatever metadata is available and
+        // understood.
+        //
+        try {
+            Metadata metadata = ImageMetadataReader.readMetadata(file);
+
+            print(metadata, "Using ImageMetadataReader");
+        } catch (ImageProcessingException e) {
+            print(e);
+        } catch (IOException e) {
+            print(e);
+        }
+    }
+
+    /**
+     * Write all extracted values to stdout.
+     */
+    private static void print(Metadata metadata, String method) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.print(' ');
+        System.out.print(method);
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+
+        //
+        // A Metadata object contains multiple Directory objects
+        //
+        for (Directory directory : metadata.getDirectories()) {
+
+            //
+            // Each Directory stores values in Tag objects
+            //
+            for (Tag tag : directory.getTags()) {
+                System.out.println(tag);
+            }
+
+            //
+            // Each Directory may also contain error messages
+            //
+            for (String error : directory.getErrors()) {
+                System.err.println("ERROR: " + error);
+            }
+        }
+    }
+
+    private static void print(Exception exception) {
+        System.err.println("EXCEPTION: " + exception);
+    }
+}
